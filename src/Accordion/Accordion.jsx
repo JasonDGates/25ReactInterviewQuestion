e5a4bfbb205cf564eea6a623edb257711c5eb7b6
Jason Gates <gatesjasond@gmail.com>
2024-02-05 21:51:58 -0600

    single answer complete

--- src/Accordion/Accordion.jsx
+++ src/Accordion/Accordion.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
 import data from "./data"
 import './styles.css'
 
 export default function Accordion() {
-  console.log(data)
+  const [visible, setVisible] = useState('0');
+  const [multiVisible, setMultiVisible] = useState(false);
+
+  const handleSingleShow = (id) => {
+    if (visible === id) {
+      setVisible('0')
+    } else {
+      setVisible(id);
+    }
+  }
+
   return <div className="wrapper">
+    <div className="card"
+    onClick={() => setMultiVisible(!multiVisible)}>
+      {
+        multiVisible ?
+        <div>Multiple Answers are allowed</div> :
+        <div>Multiple Answers are NOT allowed</div>
+      }
+    </div>
     {data.map(dataItem => (
-      <div key={dataItem.id}>{dataItem.question}</div>
+      <div key={dataItem.id} className="card"
+      onClick={() => handleSingleShow(dataItem.id)}>
+        <div className="question">{dataItem.question}</div>
+        {dataItem.id === visible && <div className="answer">{dataItem.answer}</div>}
+      </div>
+
     ))}
   </div>
 }
